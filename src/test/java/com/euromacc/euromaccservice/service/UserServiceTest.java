0337747737e8e63878d3bac3f122fd0a7fdb4783
simon <bhaliten@gmail.com>
2024-04-17 12:23:39 +0200

    user service test save

--- src/test/java/com/euromacc/euromaccservice/service/UserServiceTest.java
+++ src/test/java/com/euromacc/euromaccservice/service/UserServiceTest.java
@@ -2,6 +2,8 @@ package com.euromacc.euromaccservice.service;
 
 import com.euromacc.euromaccservice.dto.CreateUserRequest;
 import com.euromacc.euromaccservice.dto.UserResponse;
+import com.euromacc.euromaccservice.dto.UserSearchRequest;
+import com.euromacc.euromaccservice.dto.UserSearchResponse;
 import com.euromacc.euromaccservice.mapper.IUserMapper;
 import com.euromacc.euromaccservice.model.User;
 import com.euromacc.euromaccservice.repository.IUserRepository;
@@ -12,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoSettings;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -66,5 +69,38 @@ class UserServiceTest {
 
     @Test
     void findBySearchRequest() {
+        UUID id = UUID.randomUUID();
+        User savedUser = User.builder()
+                .id(id)
+                .firstName("first")
+                .lastName("last")
+                .email("email")
+                .build();
+        UserResponse mockedMapperUserResponse = UserResponse.builder()
+                .id(id)
+                .email("email")
+                .lastName("last")
+                .firstName("first")
+                .build();
+
+        Mockito.doReturn(List.of(savedUser))
+                .when(this.repository)
+                .findByFirstNameLikeAndLastNameLike(Mockito.anyString(), Mockito.anyString());
+
+        Mockito.doReturn(1L)
+                .when(this.repository)
+                .countByFirstNameLikeAndLastNameLike(Mockito.anyString(), Mockito.anyString());
+
+        Mockito.doReturn(mockedMapperUserResponse)
+                .when(this.mapper)
+                .map(Mockito.eq(savedUser));
+
+        UserSearchResponse userResponse = this.service.findBySearchRequest(UserSearchRequest.builder().firstName("first").lastName("last").build());
+
+        Assertions.assertEquals(1L, userResponse.getTotal());
+        Assertions.assertEquals(1L, userResponse.getUserList().size());
+        Assertions.assertEquals(id, userResponse.getUserList().get(0).getId());
+
+
     }
 }
