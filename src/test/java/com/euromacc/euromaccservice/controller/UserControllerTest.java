81b000ff7fad97680e6cf1e05db35de0c1edacba
simon <bhaliten@gmail.com>
2024-04-17 10:51:16 +0200

    UserControllerTest

---
+++ src/test/java/com/euromacc/euromaccservice/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.euromacc.euromaccservice.controller;
+
+import com.euromacc.euromaccservice.dto.CreateUserRequest;
+import com.euromacc.euromaccservice.dto.UserResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class UserControllerTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void save() {
+        CreateUserRequest createUserRequest = CreateUserRequest.builder()
+                .email("email@gmail.com")
+                .firstName("First")
+                .lastName("Last")
+                .build();
+
+        this.client.post()
+                .uri("/api/elasticsearch/create")
+                .bodyValue(createUserRequest)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(UserResponse.class)
+                .value(response -> {
+                    Assertions.assertEquals(createUserRequest.getEmail(), response.getEmail());
+                    Assertions.assertEquals(createUserRequest.getLastName(), response.getLastName());
+                    Assertions.assertEquals(createUserRequest.getFirstName(), response.getFirstName());
+                });
+    }
+
+    @Test
+    void search() {
+    }
+}
