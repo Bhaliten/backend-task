803d3b24e8b2a4a65a6d88f8a94f6cade15259f4
simon <bhaliten@gmail.com>
2024-04-17 11:44:06 +0200

    findByNames repo

--- src/main/java/com/euromacc/euromaccservice/repository/IUserRepository.java
+++ src/main/java/com/euromacc/euromaccservice/repository/IUserRepository.java
@@ -4,8 +4,12 @@ import com.euromacc.euromaccservice.model.User;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IUserRepository extends ElasticsearchRepository<User, UUID> {
+    List<User> findByFirstNameLikeAndLastNameLike(String firstName, String lastName);
+
+    Long countByFirstNameLikeAndLastNameLike(String firstName, String lastName);
 }
