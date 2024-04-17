a649fc74344d84fae8d82ee44551b8d85436a4cf
simon <bhaliten@gmail.com>
2024-04-17 07:40:55 +0200

    init

---
+++ src/main/java/com/euromacc/euromaccservice/EuromaccServiceApplication.java
@@ -0,0 +1,13 @@
+package com.euromacc.euromaccservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuromaccServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuromaccServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/euromacc/euromaccservice/EuromaccServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euromacc.euromaccservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuromaccServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
