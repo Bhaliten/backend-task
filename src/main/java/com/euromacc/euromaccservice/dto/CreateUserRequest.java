1c589eeda1be0412f75cd68be252faf6c6883d6c
simon <bhaliten@gmail.com>
2024-04-17 10:50:47 +0200

    builder added to CreateUserRequest

--- src/main/java/com/euromacc/euromaccservice/dto/CreateUserRequest.java
+++ src/main/java/com/euromacc/euromaccservice/dto/CreateUserRequest.java
@@ -1,9 +1,11 @@
 package com.euromacc.euromaccservice.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.lang.NonNull;
 
 @Data
+@Builder
 public class CreateUserRequest {
     @NonNull
     String firstName;
